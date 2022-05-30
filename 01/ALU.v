be0660f6837470c28892d89cc2e81f51c6e50ab7
buhe <bugu1986@126.com>
2022-05-30 15:18:51 +0800

    u

---
+++ 01/ALU.v
@@ -0,0 +1,28 @@
+module ALU (
+    x,y,zx,nx,zy,ny,f,no,out,zr,ng
+);
+input x;
+input y;
+input zx;
+input nx;
+input zy;
+input ny;
+input f;
+input no;
+
+output out;
+output zr;
+output ng;
+
+reg tempx;
+
+always @* //NOTE: You are describing combo logic, since there is no clock signal
+begin
+
+      if(zx == 0)
+        tempx = 0;
+      else 
+        tempx = x;
+end
+    
+endmodule
