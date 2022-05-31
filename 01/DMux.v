f94ab480f2090bdc27d10cc26efb52029ddcd521
buhe <bugu1986@126.com>
2022-05-31 21:54:05 +0800

    u

--- 01/DMux.v
+++ 01/DMux.v
@@ -3,6 +3,8 @@
  * {a, b} = {in, 0} if sel == 0
  *          {0, in} if sel == 1
  */
+`include "Not.v"
+`include "And.v"
 `default_nettype none
 
 module DMux(
@@ -13,8 +15,12 @@ module DMux(
 );
 
 // your implementation comes here:
-
-
-
+    // Not(in=sel, out=notsel);
+    // And(a=in, b=notsel, out=a);
+    // And(a=in, b=sel, out=b);
+    wire notsel;
+    Not NOT2(.in(sel), .out(notsel));
+	And AND1(.a(in),.b(notsel),.out(a));
+	And AND2(.a(in),.b(sel),.out(b));
 
 endmodule
