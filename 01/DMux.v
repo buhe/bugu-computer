d32dd98d337d198e3b81a34739234a99b17ab07b
buhe <bugu1986@126.com>
2022-05-31 20:53:05 +0800

    u

---
+++ 01/DMux.v
@@ -0,0 +1,20 @@
+/**
+ * Demultiplexor:
+ * {a, b} = {in, 0} if sel == 0
+ *          {0, in} if sel == 1
+ */
+`default_nettype none
+
+module DMux(
+	input wire in,
+	input wire sel,
+    output wire a,
+	output wire b
+);
+
+// your implementation comes here:
+
+
+
+
+endmodule
---
+++ 01/Mux.v
@@ -0,0 +1,17 @@
+/** 
+ * Multiplexor:
+ * out = a if sel == 0
+ *       b otherwise
+ */
+`default_nettype none
+
+module Mux(
+	input a,
+	input b,
+	input sel,
+	output out
+);
+
+
+
+endmodule
