8073b5c461377fecca8c2815490adca05e530d01
buhe <bugu1986@126.com>
2022-05-31 22:06:18 +0800

    u

--- 01/Mux.v
+++ 01/Mux.v
@@ -3,6 +3,9 @@
  * out = a if sel == 0
  *       b otherwise
  */
+`include "Not.v"
+`include "And.v"
+`include "Or.v"
 `default_nettype none
 
 module Mux(
@@ -11,7 +14,19 @@ module Mux(
 	input sel,
 	output out
 );
+    // Not(in=sel, out=notsel);
+    // And(a=a, b=notsel, out=outa);
+    // And(a=b, b=sel, out=outb);
+    // Or(a=outa, b=outb, out=out);
 
+    wire notsel;
+    Not NOT(.in(sel), .out(notsel));
+	
+	wire w1;
+	wire w2;
+	And AND1(.a(a),.b(notsel),.out(w1));
+	And AND2(.a(b),.b(sel),.out(w2));
 
+	Or OR(.a(w1),.b(w2),.out(out));
 
 endmodule
