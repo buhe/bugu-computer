964768435e935e9a7b45fd1395870430df53937c
buhe <bugu1986@126.com>
2022-06-02 10:48:38 +0800

    u

--- 01/DMux4Way.v
+++ 01/DMux4Way.v
@@ -5,7 +5,7 @@
  *                {0, 0, in, 0} if sel == 10
  *                {0, 0, 0, in} if sel == 11
  */
-
+ `include "DMux.v"
 module DMux4Way(
 	input wire in,
 	input wire [1:0] sel,
@@ -15,8 +15,14 @@ module DMux4Way(
 	output wire d
 	);
 // your implementation comes here:
-
-
+    // DMux(in=in, sel=sel[1], a=ab, b=cd);
+    // DMux(in=ab, sel=sel[0], a=a, b=b);
+    // DMux(in=cd, sel=sel[0], a=c, b=d);
+    wire ab;
+    wire cd;
+    DMux DMUX1(.in(in), .sel(sel[1]), .a(ab), .b(cd));
+    DMux DMUX2(.in(ab), .sel(sel[0]), .a(a), .b(b));
+    DMux DMUX3(.in(cd), .sel(sel[0]), .a(c), .b(d));
 
 
 endmodule
