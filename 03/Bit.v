6013127751461ecb628c34db92ad2aef8961132b
buhe <bugu1986@126.com>
2022-06-04 18:00:41 +0800

    add bit

---
+++ 03/Bit.v
@@ -0,0 +1,25 @@
+/**
+ * 1-bit register:
+ * If load[t] == 1 then out[t+1] = in[t]
+ *    else out does not change (out[t+1] = out[t])
+ */
+`include "Mux.v"
+`include "DFF.v"
+`default_nettype none
+module Bit(
+	input wire clk,
+	input wire in,
+	input wire load,
+	output wire out
+);
+
+// your implementation comes here:
+  
+    // Mux(a=muxb, b=in, sel=load, out=muxo);
+    // DFF(in=muxo, out=out, out=muxb);
+    // reg muxb;
+    wire muxo;
+    Mux MUX(.a(out),.b(in),.sel(load),.out(muxo));
+	DFF DFF1(.clk(clk),.in(muxo),.out(out));
+
+endmodule
---
+++ 03/Mux.v
@@ -0,0 +1,31 @@
+/** 
+ * Multiplexor:
+ * out = a if sel == 0
+ *       b otherwise
+ */
+`default_nettype none
+`include "Not.v"
+`include "And.v"
+`include "Or.v"
+module Mux(
+	input a,
+	input b,
+	input sel,
+	output out
+);
+    // Not(in=sel, out=notsel);
+    // And(a=a, b=notsel, out=outa);
+    // And(a=b, b=sel, out=outb);
+    // Or(a=outa, b=outb, out=out);
+
+    wire notsel;
+    Not NOT(.in(sel), .out(notsel));
+	
+	wire w1;
+	wire w2;
+	And AND1(.a(a),.b(notsel),.out(w1));
+	And AND2(.a(b),.b(sel),.out(w2));
+
+	Or OR(.a(w1),.b(w2),.out(out));
+
+endmodule
