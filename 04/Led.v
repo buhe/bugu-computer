6bcf6b3fb8e242b39be315db7fc34d3cd2bdfbd9
buhe <bugu1986@126.com>
2022-06-07 16:55:08 +0800

    fix bug

--- 04/Led.v
+++ 04/Led.v
@@ -8,8 +8,15 @@ module Led(
 	input wire[15:0] in
 );
     wire prev;
-    Mux MUX(.a(out[0]),.b(in[0]),.sel(load),.out(prev));
+    Mux MUX(.a(outLow),.b(in[0]),.sel(load),.out(prev));
     assign led = prev;
-	DFFusr DFF1(.clk(clk),.in(prev),.out(out[0]));
+    wire outLow;
+	DFFusr DFF1(.clk(clk),.in(prev),.out(outLow));
+          Mux16 MUX161(
+		.a(16'b0000000000000000),
+		.b(16'b0000000000000001),
+		.sel(outLow),
+	    .out(out)
+	  );
 
 endmodule
