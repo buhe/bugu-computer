37b3b964a84106128bc4f274f00f235e9b56a388
buhe <bugu1986@126.com>
2022-06-08 10:32:32 +0800

    u

--- 04/Computer.v
+++ 04/Computer.v
@@ -1,14 +1,49 @@
 `default_nettype none
+`include "Memory.v"
+`include "CPU.v"
+`include "ROM.v"
 module Computer( 
     input clk_in,				// external clock 100 MHz	
     input btn,			// buttons	(0 if pressed, 1 if released)
-	output led			// leds 	(0 off, 1 on)
+	output led,			// leds 	(0 off, 1 on)
+    input reset
 );
 	
     // ROM32K(address=pc ,out=instruction );
     // CPU(inM=Mout ,instruction=instruction ,reset=reset ,outM=outM ,writeM=writeM ,addressM=addressM ,pc=pc );
     // Memory(in=outM ,load=writeM ,address=addressM ,out=Mout );
 
+    wire [15:0] addressM;
+    wire [15:0] outM;
+    wire [15:0] instruction;
+    wire [15:0] pc;
+    wire [15:0] Mout;
 
+    wire writeM;
+
+	ROM ROM(
+		.instruction(instruction),
+		.pc(pc)
+	);
+	CPU CPU(
+		.clk(clk_in),
+		.inM(Mout),
+		.instruction(instruction),
+		.reset(reset),
+		.outM(outM),
+		.writeM(writeM),
+		.addressM(addressM),
+		.pc(pc)
+	);
+
+	Memory MEMORY(
+		.clk(clk_in),
+		.address(addressM),
+		.in(outM),
+		.out(Mout),
+		.load(writeM),
+        .btn(btn),
+        .led(led)
+	);
 
 endmodule
--- 04/Computer_tb.v
+++ 04/Computer_tb.v
@@ -4,12 +4,14 @@ module Computer_tb();
 reg clk_in = 0;
 reg btn = 0;
 wire led;
+reg reset = 1;
 
 Computer
   HACK1(
     .clk_in(clk_in),
     .btn(btn),
-    .led(led)
+    .led(led),
+    .reset(reset)
   );
 
 always #1 clk_in = ~clk_in;
